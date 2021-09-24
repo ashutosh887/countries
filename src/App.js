07d564a0fa81ca9be8579d9c311f0114fb99417a
ashutosh887 <ashutoshj887@gmail.com>
2021-09-24 18:58:39 +0530

    Components added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import Countries from './components/Countries';
 
 function App() {
   return (
-      <h1>Ashutosh Jha</h1>
+    <Countries />
   );
 }
 
---
+++ src/components/Countries.js
@@ -0,0 +1,13 @@
+import React, {useState, useEffect} from 'react'
+
+const url = "https://restcountries.com/";
+
+const Countries = () => {
+    return (
+        <div>
+            India is my Country and all Indians are my Brother and Sister
+        </div>
+    )
+}
+
+export default Countries
