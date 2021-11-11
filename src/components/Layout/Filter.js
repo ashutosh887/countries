9fc8eaed61adbeeaf6acfcd21a085224827a7a94
ashutosh887 <ashutoshj887@gmail.com>
2021-11-11 23:53:31 +0530

    updated header filter

--- src/components/Layout/Filter.js
+++ src/components/Layout/Filter.js
@@ -1,26 +1,103 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-const Filter = () => {
+const Filter = ({
+  searchInput,
+  setSearchInput,
+  setFiltered,
+  setCountries,
+  countries,
+}) => {
+  const regions = [
+    {
+      name: "Filter by region",
+      desc: "All",
+    },
+    {
+      name: "Africa",
+      desc: "Africa",
+    },
+    {
+      name: "Americas",
+      desc: "Americas",
+    },
+    {
+      name: "Asia",
+      desc: "Asia",
+    },
+    {
+      name: "Europe",
+      desc: "Europe",
+    },
+    {
+      name: "Oceania",
+      desc: "Oceania",
+    },
+  ];
+
+  // Prevent page reload when submitting the form
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  // Search countries
+  const searchCountries = (searchValue) => {
+    setSearchInput(searchValue);
+
+    if (searchInput) {
+      const filteredCountries = countries.filter((country) =>
+        Object.values(country)
+          .join("")
+          .toLowerCase()
+          .includes(searchValue.toLowerCase())
+      );
+      setFiltered(filteredCountries);
+    } else {
+      setFiltered(countries);
+    }
+  };
+
+  // Filter by region
+
+  const filterRegions = async (region) => {
+    const url = `https://restcountries.com/v2/region/${region}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    setCountries(data);
+  };
+
+  useEffect(() => {
+    filterRegions();
+    // eslint-disable-next-line
+  }, []);
+
   return (
-    <section className="filter">
-      <form className="form-control">
+    <>
+      <form className="form" id="form" onSubmit={handleSubmit}>
         <input
-          type="text"
+          type="search"
           name="search"
-          placeholder="Search for a country..."
           id="search"
+          autoComplete="off"
+          placeholder="Search Country"
+          onChange={(e) => searchCountries(e.target.value)}
         />
+
+        <div className="select">
+          <select
+            name="select"
+            id="select"
+            onChange={(e) => filterRegions(e.target.value)}
+            value={regions.name}
+          >
+            <option value="Africa">Africa</option>
+            <option value="Asia">Asia</option>
+            <option value="Europe">Europe</option>
+            <option value="Americas">America</option>
+            <option value="Oceania">Oceania</option>
+          </select>
+        </div>
       </form>
-      <div>
-        <select name="select" id="select" className="select">
-          <option value="region">Filter</option>
-          <option value="Africa">Africa</option>
-          <option value="Asia">Asia</option>
-          <option value="Europe">Europe</option>
-          <option value="Oceania">Oceania</option>
-        </select>
-      </div>
-    </section>
+    </>
   );
 };
 
